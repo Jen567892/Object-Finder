e55f81cdb431fb0a8dbfeaddc3319ff33364e257
Jen567892 <82474362+Jen567892@users.noreply.github.com>
2021-10-04 18:16:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(640, 420)
+canvas.center()
+
+video = createCapture(VIDEO)
+video.hide()
+}
+
+function draw(){
+    image(video, 0, 0, 640, 420)
+}
+
+
